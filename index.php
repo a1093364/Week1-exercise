2884d194e3f10cb486bbcdba2eb21633f9439d3c
a1093364 <100330639+a1093364@users.noreply.github.com>
2022-02-24 23:33:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    </head>
+        <body>
+            <?php
+            $i=777;
+            $j=666;
+            echo "<b>Hello I am a1093364.</b> How are you? <h1>PHP good!</h1>";
+            echo "<br/>";
+            echo $i+$j;
+            echo "<br/>";
+            echo date("y,m,d");
+            ?>
+        </body>
+</html>
